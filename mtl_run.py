d6598813b205a4db6dcba72f2696a5f3e770aa51
wdxwj <wdxwj@vip.qq.com>
2021-03-02 14:46:30 +0800

    Add utils

---
+++ mtl_run.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import time
+import argparse
+import importlib
+import collections
+import numpy as np
+import multiprocessing
+
+
+from utils.configure import PDConfig
+
+def train(multitask_config): 
+    print("Loading multi_task configure...................")
+
+
+
+if __name__ == '__main__':
+
+    multitask_config = "mtl_config.yaml"
+    train(multitask_config)
